8e852623221d793cd64c020719e34472d44c6027
plombir1917 <dfhhggh@list.ru>
2024-02-16 10:13:26 +0300

    feat: file-system package, html downloading

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { error } from 'console';
+import * as fs from 'fs';
 
 @Injectable()
 export class AppService {
   async downloadPage(url: string, proxy?: string) {
     try {
       const response = await axios.get(url);
+      const data = response.data;
+      const fileName = 'downloaded.html';
+      fs.writeFileSync(fileName, data);
       return `Downloaded ${url}, status code: ${response.status}`;
     } catch (error) {
-      throw new error(`Error downloading ${url}: ${error.message}`);
+      throw new Error(`Error downloading ${url}: ${error.message}`);
     }
   }
 
