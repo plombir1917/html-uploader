c49c5a343f146787399c911be20f9e1363566b6f
Zakhar Kopich <114385028+plombir1917@users.noreply.github.com>
2024-02-16 14:30:04 +0300

    feat: Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,11 +3,47 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, objectConstructor, objectReseter, initialSize = 5000) {
+    this.objectConstructor = objectConstructor;
+    this.objectReseter = objectReseter;
+    this._pool = [];
+    for (let i = 0; i < initialSize; i++) {
+      this._addObjectToPool();
+    }
+  }
 
+  _addObjectToPool() {
+    const newObj = {
+        alive: false,
+        data: this.objectConstructor()
+      };
+    this._pool.push(newObj);
+  }
+
+  _allocate(object) {
+    object.alive = true;
+    return object;
+  }
+
+  getNew() {
+    for (let i = 0; i < this._pool.length; i++) {
+      if (this._pool[i].alive === false) {
+        return this._allocate(this._pool[i]);
+      }
+    }
+    return this._addObjectToPool();
+  }
+
+  release(object) {
+    object.alive = false;
+  }
+  
   @Post('upload')
   async downloadPages(@Body() data: { urls: string[]; proxies?: string[] }) {
     const { urls, proxies } = data;
-    return await this.appService.downloadPages(urls, proxies);
+    return await this.appService.downloadPages(object({
+      url: string[],
+      proxies: string[]
+    });
   }
 }
